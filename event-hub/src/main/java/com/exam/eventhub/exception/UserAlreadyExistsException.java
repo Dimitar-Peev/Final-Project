c0b051738892c5f10b8238eca01b16a115474120
Dimitar-Peev <mctrix87@abv.bg>
2025-10-26 19:11:23 +0200

    Created UserAlreadyExistsException.java

---
+++ event-hub/src/main/java/com/exam/eventhub/exception/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.exam.eventhub.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
