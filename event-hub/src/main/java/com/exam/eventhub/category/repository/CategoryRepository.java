c0c578f7714434080153302e06cb506fa1b9fb1e
Dimitar-Peev <mctrix87@abv.bg>
2025-10-27 17:53:35 +0200

    Created CategoryRepository.java

---
+++ event-hub/src/main/java/com/exam/eventhub/category/repository/CategoryRepository.java
@@ -0,0 +1,15 @@
+package com.exam.eventhub.category.repository;
+
+import com.exam.eventhub.category.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+    Optional<Category> findByName(String name);
+
+}
