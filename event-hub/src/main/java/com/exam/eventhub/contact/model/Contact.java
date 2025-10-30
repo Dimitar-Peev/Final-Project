633d067bb2f3f1fbc4498c5e265c2320b6b662b1
Dimitar-Peev <mctrix87@abv.bg>
2025-10-30 18:29:58 +0200

    Created Contact.java

---
+++ event-hub/src/main/java/com/exam/eventhub/contact/model/Contact.java
@@ -0,0 +1,33 @@
+package com.exam.eventhub.contact.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
