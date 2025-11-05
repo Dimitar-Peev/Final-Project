460cc08dcb4e2710b192d92e816c2dcbef4490a9
Dimitar-Peev <mctrix87@abv.bg>
2025-11-05 17:23:33 +0200

    Created ContactCreateRequest.java

---
+++ event-hub/src/main/java/com/exam/eventhub/web/dto/ContactCreateRequest.java
@@ -0,0 +1,26 @@
+package com.exam.eventhub.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ContactCreateRequest {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Email(message = "Please provide a valid email")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Subject is required")
+    private String subject;
+
+    @NotBlank(message = "Message is required")
+    private String message;
+}
