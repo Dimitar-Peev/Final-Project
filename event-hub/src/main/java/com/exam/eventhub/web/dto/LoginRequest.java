2a2592b52e6be482187d91c2cc2c789954e51a6d
Dimitar-Peev <mctrix87@abv.bg>
2025-11-05 17:19:36 +0200

    Created LoginRequest.java

---
+++ event-hub/src/main/java/com/exam/eventhub/web/dto/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.exam.eventhub.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters!")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 60, message = "Password must be at least 6 characters long")
+    private String password;
+}
+
