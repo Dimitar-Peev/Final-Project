269a046278c2c672c5c1741fb4c975f0bf5e681c
Dimitar-Peev <mctrix87@abv.bg>
2025-12-04 17:47:34 +0200

    Created AdminBookingController.java

---
+++ event-hub/src/main/java/com/exam/eventhub/web/AdminBookingController.java
@@ -0,0 +1,45 @@
+package com.exam.eventhub.web;
+
+import com.exam.eventhub.booking.service.BookingService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.UUID;
+
+import static com.exam.eventhub.common.Constants.SUCCESS_MESSAGE_ATTR;
+
+@Controller
+@RequestMapping("/admin/bookings")
+@AllArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
+public class AdminBookingController {
+
+    private final BookingService bookingService;
+
+    @GetMapping
+    public String listAllBookings(Model model) {
+        model.addAttribute("bookings", bookingService.getAllBookings());
+        return "admin/manage-bookings";
+    }
+
+    @PostMapping("/{id}/cancel")
+    public String cancelBooking(@PathVariable UUID id, RedirectAttributes redirectAttributes) {
+        bookingService.adminCancelBooking(id);
+        redirectAttributes.addFlashAttribute(SUCCESS_MESSAGE_ATTR, "Booking cancelled!");
+        return "redirect:/admin/bookings";
+    }
+
+    @PostMapping("/{id}/refund")
+    public String refundBooking(@PathVariable UUID id, RedirectAttributes redirectAttributes) {
+        bookingService.refundBooking(id);
+        redirectAttributes.addFlashAttribute(SUCCESS_MESSAGE_ATTR, "Booking refunded!");
+        return "redirect:/admin/bookings";
+    }
+}
