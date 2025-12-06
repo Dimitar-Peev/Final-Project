87993ad3a2fb2a09639dc087aaede65e1dfaeddf
Dimitar-Peev <mctrix87@abv.bg>
2025-12-06 15:55:30 +0200

    Created ApplicationTests.java

---
+++ event-hub/src/test/java/com/exam/eventhub/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.exam.eventhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
