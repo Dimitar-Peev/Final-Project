0047415601c7f3a8ade5b433aac80e26d43faf38
Dimitar-Peev <mctrix87@abv.bg>
2025-12-06 19:34:37 +0200

    Created BookingNotFoundException.java

---
+++ event-hub/src/main/java/com/exam/eventhub/exception/BookingNotFoundException.java
@@ -0,0 +1,7 @@
+package com.exam.eventhub.exception;
+
+public class BookingNotFoundException extends RuntimeException {
+    public BookingNotFoundException(String message) {
+        super(message);
+    }
+}
