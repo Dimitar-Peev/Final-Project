f3546727a7e2a93bd90f601f2ce70d5c5b93e873
Dimitar-Peev <mctrix87@abv.bg>
2025-12-07 17:08:55 +0200

    Created TransactionRepository.java

---
+++ eventhub-payment-service/src/main/java/com/exam/app/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.exam.app.repository;
+
+import com.exam.app.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+}
