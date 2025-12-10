40c74bcd5045eeb5364c2e95e395c4dd7a98e5f3
Dimitar-Peev <mctrix87@abv.bg>
2025-12-10 18:57:34 +0200

    Created TestMvcConfig.java

---
+++ event-hub/src/test/java/com/exam/eventhub/config/TestMvcConfig.java
@@ -0,0 +1,36 @@
+package com.exam.eventhub.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@TestConfiguration
+public class TestMvcConfig {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver("lang");
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setDefaultLocale(Locale.ENGLISH);
+        return messageSource;
+    }
+}
