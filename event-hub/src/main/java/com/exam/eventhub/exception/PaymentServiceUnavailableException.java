011cca82a1452c0a0cff26c925f5d1a62af8ac9c
Dimitar-Peev <mctrix87@abv.bg>
2025-12-11 15:01:40 +0200

    Created PaymentServiceUnavailableException.java

---
+++ event-hub/src/main/java/com/exam/eventhub/exception/PaymentServiceUnavailableException.java
@@ -0,0 +1,7 @@
+package com.exam.eventhub.exception;
+
+public class PaymentServiceUnavailableException extends RuntimeException {
+    public PaymentServiceUnavailableException(String message) {
+        super(message);
+    }
+}
