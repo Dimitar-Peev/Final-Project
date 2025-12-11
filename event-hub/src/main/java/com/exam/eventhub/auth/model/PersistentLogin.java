819e5718e7fb056dbe15630f5720f33e5ce0aa12
Dimitar-Peev <mctrix87@abv.bg>
2025-12-11 05:12:03 +0200

    Created PersistentLogin.java

---
+++ event-hub/src/main/java/com/exam/eventhub/auth/model/PersistentLogin.java
@@ -0,0 +1,34 @@
+package com.exam.eventhub.auth.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "persistent_logins")
+public class PersistentLogin {
+
+    @Id
+    @Column(length = 64)
+    private String series;
+
+    @Column(nullable = false, length = 64)
+    private String username;
+
+    @Column(nullable = false, length = 64)
+    private String token;
+
+    @Column(name = "last_used", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime lastUsed;
+}
